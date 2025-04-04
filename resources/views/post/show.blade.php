45c2268a486ff905be9b8d53e59d127965c23095
Kristaps <Kristaps@gmail.com>
2025-04-04 11:33:12 +0300

    Show view add if statement to check user

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -24,6 +24,7 @@
                         @endif
                     </div>
 
+                    @if ($post->user_id === auth()->id())
                     <div class="min-w-fit min-h-fit my-3">
                         <a href="{{ route('posts.edit', $post->id) }}" 
                             class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900"
@@ -39,6 +40,7 @@
                             </button>
                         </form>
                     </div>
+                    @endif
 
                     <h2>Comments:</h2>
                     @foreach ($post->comments as $comment)
